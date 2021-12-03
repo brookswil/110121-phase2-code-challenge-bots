4abb56717f59248918650bc3853dbb61efdd18d0
Brooks_Wilcox <brookswill88@gmail.com>
2021-12-03 12:11:06 -0700

    submission for deadline

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
+
+
+const API = 'http://localhost:8002/bots';
 
 function BotsPage() {
   //start here with your code for step one
 
+  const [bots, setBots] = useState([]);
+
+  useEffect(() => {
+    fetch(API)
+      .then(res => res.json())
+      .then(setBots);
+  }, []);
+
+  function enlistBot(bot) {
+    setBots(bots.map((b) => (b.id === bot.id ? {...b, enlist: true } : b )));
+  }
+
+  function releaseBot(bot) {
+    setBots(bots.map((b) => (b.id === bot.id ? {...b, enlist: false } : b )));
+  }
+
+  function dischargeBot(bot) {
+    console.log('releasing', bot);
+    setBots(bots.filter((b) => b.id !== bot.id));
+  }
+
   return (
     <div>
-      <YourBotArmy />
-      <BotCollection />
+      <YourBotArmy bots={bots.filter(b => b.enlist)} releaseBot={releaseBot} dischargeBot={dischargeBot}/>
+      <BotCollection bots={bots} enlistBot={enlistBot} dischargeBot={dischargeBot}/>
     </div>
   )
 }
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,13 +1,16 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function YourBotArmy() {
+function YourBotArmy({bots, releaseBot, dischargeBot}) {
   //your bot army code here...
 
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
-          {/*...and here...*/}
+        {bots.map((bot) => (
+          <BotCard key={bot.id} bot={bot} handleClick={releaseBot} dischargeBot={dischargeBot}/>
+        ))}
           Your Bot Army
         </div>
       </div>
